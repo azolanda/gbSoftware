c5c5b1a50e52afb64fd9417eb144c885f14f62ee
azolanda <uboikov@yandex.ru>
2023-12-05 19:04:31 +0300

    classes Camera and Scene done

--- lesson1/ModelElements/Camera.java
+++ lesson1/ModelElements/Camera.java
@@ -2,7 +2,31 @@ package lesson1.ModelElements;
 
 /**
  * Камера
- * TODO: Доработать самостоятельно в рамках домашней работы
  */
 public class Camera {
+
+    private Point3D location;
+
+    private Angle3D angle;
+
+    public Camera(Point3D location, Angle3D angle) {
+        this.location = location;
+        this.angle = angle;
+    }
+
+    public Point3D getLocation() {
+        return location;
+    }
+
+    public Angle3D getAngle() {
+        return angle;
+    }
+
+    public void rotate(Angle3D angle3d) {
+        System.out.println("rotate " + angle3d);
+    }
+
+    public void move(Point3D point3d) {
+        System.out.println("move " + point3d);
+    }
 }
--- lesson1/ModelElements/Scene.java
+++ lesson1/ModelElements/Scene.java
@@ -1,8 +1,55 @@
 package lesson1.ModelElements;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Сцена
- * TODO: Доработать самостоятельно в рамках домашней работы
  */
 public class Scene {
+
+    private int counter = 1;
+
+    private int id;
+
+    private Collection<PoligonalModel> models = new ArrayList<>();
+
+    private Collection<Flash> flashes = new ArrayList<>();
+
+    private Collection<Camera> cameras = new ArrayList<>();
+
+    public Scene(PoligonalModel model, Flash flash, Camera camera) {
+        this.models.add(model);
+        this.flashes.add(flash);
+        this.cameras.add(camera);
+        id = ++counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Collection<PoligonalModel> getModels() {
+        return models;
+    }
+
+    public Collection<Flash> getFlashes() {
+        return flashes;
+    }
+
+    public Collection<Camera> getCameras() {
+        return cameras;
+    }
+
+    public void addModel(PoligonalModel model) {
+        this.models.add(model);
+    }
+
+    public void addFlash(Flash flash) {
+        this.flashes.add(flash);
+    }
+
+    public void addCamera(Camera camera) {
+        this.cameras.add(camera);
+    }
 }
