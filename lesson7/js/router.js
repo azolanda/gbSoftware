facb860a2c23e6c096eeb82016dee91bcc4372e7
azolanda <uboikov@yandex.ru>
2023-12-20 20:19:11 +0300

    homework7 some changes

--- lesson7/js/router.js
+++ lesson7/js/router.js
@@ -2,22 +2,22 @@
 
 const routes = {
     "/lesson7/" : "/lesson7/pages/home.html",
-    "/lesson7/settings" : "/lesson7/pages/settings.html",
-    "/lesson7/about" : "/lesson7/pages/about.html",
-    "/lesson7/products" : "/lesson7/pages/products.html",
-    "/lesson7/contacts" : "/lesson7/pages/contacts.html",
+    "/lesson7/#/settings" : "/lesson7/pages/settings.html",
+    "/lesson7/#/about" : "/lesson7/pages/about.html",
+    "/lesson7/#/products" : "/lesson7/pages/products.html",
+    "/lesson7/#/contacts" : "/lesson7/pages/contacts.html",
+    "/lesson7/#/home" : "/lesson7/pages/home.html",
     404 : "/lesson7/pages/404.html", 
 }
 
 const route = (event) => {
-    // event = event || window.event;
     event.preventDefault();
     window.history.pushState({}, "", event.target.href);
     handleLocation();
 };
 
 const handleLocation = async () => {
-    const path = window.location.pathname;
+    const path = window.location.pathname + window.location.hash;
     const route = routes[path] || routes[404];
     const html = await fetch(route).then((page) => page.text());
     document.getElementById("main-page").innerHTML = html;
@@ -25,7 +25,6 @@ const handleLocation = async () => {
 
 window.onpopstate = handleLocation;
 
-// window.route = route;
 handleLocation();
 
 const setYear = () => {
